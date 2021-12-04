c6628a22317a79468f7d0411d630506e6ba1da61
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2021-12-04 17:11:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/MdNoor786/SessionBot")
 os.system("cd sessionhack_bot && python bot.py")
